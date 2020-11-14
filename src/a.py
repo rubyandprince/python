b6f50a6cb5d40704fbefb26c9cea4bf43ebcf0f5
rubyandprince <ruby@twgreensigns.com>
2020-11-14 04:04:53 +0000

    1

---
+++ src/a.py
@@ -0,0 +1,15 @@
+# num = input().split()
+# b = []
+# for i in range(5):
+#     num[i] = int(num[i])
+#     b.append(num[i] + 10)
+# print(b)
+
+my_list = input().split()
+num = int(input())
+a = []
+for i in range(num):
+    n = int(input())
+    a.append(n)
+print(my_list)
+print(my_list + a)
